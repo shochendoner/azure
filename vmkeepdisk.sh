b8e3f9d672cc8b87ba9c448e6c7175e5b4dd33ba
shochendoner@gmail.com <shochendoner@gmail.com>
2022-09-07 15:58:54 -0400

    save

--- vmkeepdisk.sh
+++ vmkeepdisk.sh
@@ -1,3 +1,3 @@
 RG = 'cloud-test-rg02'
 VM = 'LamarJackson-VM'
-az resource update --resource-group 'sphereus' --name 'AvePointVM01' --resource-type virtualMachines --namespace Microsoft.Compute --set properties.storageProfile.osDisk.deleteOption=detach
+az resource update --resource-group 'sphereus' --name 'SPHEREExch2010' --resource-type virtualMachines --namespace Microsoft.Compute --set properties.storageProfile.osDisk.deleteOption=detachw

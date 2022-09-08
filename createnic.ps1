d2a88747f112f286e46b1a1a89d5d99c970effb2
shochendoner <shochendoner@gmail.com>
2022-09-07 20:40:15 -0400

    finally

--- createnic.ps1
+++ createnic.ps1
@@ -1,6 +1,7 @@
 $VMs = Import-csv -path '.\vms.csv' 
-$nicname = $VMs.VMname + "-nic01"
+
 foreach ($VMName in $VMs){
-    $nicname = $VMs.VMname + "-nic01"
-    New-AzNetworkInterface -Name $nicname -ResourceGroupName $VMs.RGname -Location "eastus" -SubnetId $VMs.subnet -IpConfigurationName "IPConfiguration1" -PublicIpAddressId 'None' 
+    New-AzNetworkInterface -Name $VMs.nicname -ResourceGroupName $VMs.RGname -Location "eastus" -SubnetId $VMs.subnet -IpConfigurationName "IPConfiguration1" -PublicIpAddressId 'None' 
 }
+
+

c9c57b899417175a1ab3ea734e2b099561a3b4de
shochendoner@gmail.com <shochendoner@gmail.com>
2022-10-03 08:58:20 -0400

    sdfadsfasdf

--- new/deploy_sales.ps1
+++ new/deploy_sales.ps1
@@ -1,6 +1,6 @@
 $templateFile = ".\azuredeploy.sales.json"
 $parameterFile = ".\param\SB04-DEMO.azuredeploy.parameters.json"
-$rgname = "Network-SALES-rg"
+$rgname = "SB-SALES-rg"
 New-AzResourceGroupDeployment `
   -Name sb25-uatmove02 `
   -ResourceGroupName $rgname `

ab7fd3338ef107e6c00331513494e9ed71dae019
shochendoner@gmail.com <shochendoner@gmail.com>
2022-10-03 08:24:59 -0400

    dfdsfsdgfds

--- createnic.ps1
+++ createnic.ps1
@@ -1,4 +1,4 @@
-$VMs = Import-csv -path '.\new\ravensvms.csv'
+$VMs = Import-csv -path '.\vms.csv'
 
 foreach ($VMName in $VMs){
     New-AzNetworkInterface -Name $VMs.nic -ResourceGroupName $VMs.RGname -Location "eastus" -SubnetId $VMs.subnet -IpConfigurationName "IPConfiguration1" -PublicIpAddressId 'None' 

d91654980b54164e933b1ac224f4564523baab13
shochendoner@gmail.com <shochendoner@gmail.com>
2022-10-17 14:57:27 -0400

    dsgadgagdgdsa

--- new/deploy_uat.ps1
+++ new/deploy_uat.ps1
@@ -1,8 +1,8 @@
 $templateFile = ".\azuredeploy.uat.json"
-$parameterFile = ".\param\SB04-UAT.azuredeploy.parameters.json"
+$parameterFile = ".\param\SB11-UAT.azuredeploy.parameters.json"
 $rgname = "Network-UAT-rg"
 New-AzResourceGroupDeployment `
-  -Name UATmove02 `
+  -Name SB06-uatday2 `
   -ResourceGroupName $rgname `
   -TemplateFile $templateFile `
   -TemplateParameterFile $parameterFile

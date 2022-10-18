3a1de5ab6eb38ab6666b6960425248a1e69bdde3
shochendoner <shochendoner@gmail.com>
2022-10-17 20:52:44 -0400

    fdgdfs

--- new/deploy_uat.ps1
+++ new/deploy_uat.ps1
@@ -1,8 +1,8 @@
 $templateFile = ".\azuredeploy.uat.json"
-$parameterFile = ".\param\SB04-UAT.azuredeploy.parameters.json"
+$parameterFile = ".\param\SB16-UAT.azuredeploy.parameters.json"
 $rgname = "Network-UAT-rg"
 New-AzResourceGroupDeployment `
-  -Name UATmove02 `
+  -Name saturdaystuffUAT `
   -ResourceGroupName $rgname `
   -TemplateFile $templateFile `
   -TemplateParameterFile $parameterFile

e094f29d98d86f7f43de65c5ea4f9484fe6f4a21
shochendoner@gmail.com <shochendoner@gmail.com>
2022-10-19 08:56:34 -0400

    commit

--- new/deploy.ps1
+++ new/deploy.ps1
@@ -1,6 +1,6 @@
 $templateFile = ".\azuredeploy.json"
-$parameterFile = ".\param\SB25-UAT.azuredeploy.parameters.json"
-$rgname = "Network-UAT-rg"
+$parameterFile = ".\param\SB11-UAT.azuredeploy.parameters.json"
+$rgname = "SPHEREUS"
 New-AzResourceGroupDeployment `
   -Name sb25-uatmove02 `
   -ResourceGroupName $rgname `

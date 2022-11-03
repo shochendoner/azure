9219f5afde82eba2cd8ae9b62d2205855f69f8fd
shochendoner@gmail.com <shochendoner@gmail.com>
2022-11-03 16:19:49 -0400

    steveveveevev

--- createnic.ps1
+++ createnic.ps1
@@ -1,7 +1,7 @@
 $VMs = Import-csv -path '.\vms.csv'
 
 foreach ($VMName in $VMs){
-    New-AzNetworkInterface -Name $VMs.nic -ResourceGroupName $VMs.RGname -Location "eastus" -SubnetId $VMs.subnet -IpConfigurationName "IPConfiguration1" -PublicIpAddressId 'None' 
+    New-AzNetworkInterface -Name $VMs.nic -ResourceGroupName 'Network-Corp-rg' -Location "eastus" -SubnetId "/subscriptions/738ed618-66b1-437e-a2af-768880a84417/resourceGroups/Network-Corp-rg/providers/Microsoft.Network/virtualNetworks/CorporateResources-VNET/subnets/default" -IpConfigurationName "IPConfiguration1" -PublicIpAddressId 'None' 
 }
 
 
